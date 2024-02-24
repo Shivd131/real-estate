c9cc165d65c4a9350bbeb9917177beb799039e5c
Shivd131 <shivdeshpande131@gmail.com>
2024-02-24 23:13:15 +0530

    feat: sign in functionality

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,7 +1,115 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Input } from "@nextui-org/react";
+import { EyeFilledIcon } from "../assets/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "../assets/EyeSlashFilledIcon";
+import { Button } from "@nextui-org/react";
+import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
-export default function SignIn() {
-  return (
-    <div>SignIn</div>
-  )
+interface FormValues {
+  email: string;
+  password: string;
 }
+
+const SignIn: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const initialValues: FormValues = {
+    email: '',
+    password: ''
+  };
+
+  const navigate = useNavigate();
+
+  const validate = (values: FormValues) => {
+    const errors: Partial<FormValues> = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    if (!values.password) {
+      errors.password = 'Required';
+    } else if (values.password.length < 6) {
+      errors.password = 'Password must be at least 6 characters long';
+    }
+    return errors;
+  };
+
+  const handleSubmit = async (values: FormValues, { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }) => {
+    try {
+      const response = await axios.post('/api/auth/signin', values);
+      console.log(response.data);
+      setSubmitting(false)
+      navigate('/'); // Assuming after successful sign-in, user will be redirected to the dashboard
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      // Display a toast notification
+      toast.error('An error occurred. Please try again later.', {
+        //position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000 // Set the duration for which the toast will be displayed
+      });
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+
+      <Formik
+        initialValues={initialValues}
+        validate={validate}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className='flex flex-col gap-5 bg-gray-300 p-10 rounded-lg'>
+            <Field
+              type="email"
+              name="email"
+              as={Input}
+              label="Email"
+              labelPlacement="outside"
+            />
+            <ErrorMessage name="email" component="div" className='text-red-500 text-sm pr-2 -top-10' />
+            <Field
+              type={isVisible ? "text" : "password"}
+              name="password"
+              as={Input}
+              label="Password"
+              labelPlacement="outside"
+              endContent={
+                <button className="focus:outline-none" type="button" onClick={toggleVisibility} >
+                  {isVisible ? (
+                    <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+            />
+            <ErrorMessage name="password" component="div" className='text-red-500 text-sm pr-2 -top-10' />
+            <Button color="success" type="submit" className='text-white font-bold text-lg mt-5' disabled={isSubmitting} isLoading={isSubmitting}>
+              {isSubmitting ? 'Logging in...' : 'SIGN IN'}
+            </Button>
+          </Form>
+        )}
+      </Formik>
+
+      <div className='flex gap-2 mt-3'>
+        <p>Don't have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
