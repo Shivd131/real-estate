4a0636725c0615bee439616b945e0e20024b7625
Shivd131 <shivdeshpande131@gmail.com>
2024-02-25 13:43:03 +0530

    feat: add redux persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { NextUIProvider } from '@nextui-org/react'
-import { store } from './redux/store.ts'
+import { persistor, store } from './redux/store.ts'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <NextUIProvider>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </NextUIProvider>
   </Provider>,
 )
